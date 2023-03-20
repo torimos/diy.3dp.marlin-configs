3b4d0a03a278db663e66ae3edd922e63966f6871
torimos <oleg.v.lytvynenko@gmail.com>
2023-03-20 02:41:21 +0200

    leveling and tuning

--- Mk3/v2.1.2/Marlin/Configuration.h
+++ Mk3/v2.1.2/Marlin/Configuration.h
@@ -1160,7 +1160,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 518.518, 500 } //was 80,80,400,500
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 } //was 80,80,400,500
 
 
 // 400 = 27
@@ -1693,8 +1693,8 @@
 #define Y_BED_SIZE 210
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -18
-#define Y_MIN_POS -8
+#define X_MIN_POS -37
+#define Y_MIN_POS -26
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1867,8 +1867,8 @@
  *   With an LCD controller the process is guided step-by-step.
  */
 //#define AUTO_BED_LEVELING_3POINT
-#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_BILINEAR
+//#define AUTO_BED_LEVELING_LINEAR
+#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
